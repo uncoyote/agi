36ffb9f9481d4faba445f6b9262b16a3958544d6
uncoyote <jeker.git@outlook.de>
2023-08-11 13:47:31 +0200

    constraints

---
+++ constr/compile.sh
@@ -0,0 +1,3 @@
+java -jar ~/tools/ili2c-5.2.2/ili2c.jar \
+--modeldir "http://models.interlis.ch/" \
+$(pwd)/Constraints.ili
--- constr/export.sh
+++ constr/export.sh
@@ -1,3 +1,3 @@
 java -jar ~/tools/ili2gpkg-4.9.1/ili2gpkg-4.9.1.jar \
 --export --dbfile $(pwd)/tmp/constraints.gpkg --modeldir %ILI_FROM_DB --models Constraints \
-$(pwd)/export.xtf
+$(pwd)/data.xtf
