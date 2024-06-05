10d51c0ae1228967531598cfcf36d770c81fba18
Oliver <jeker.git@outlook.de>
2024-06-05 15:12:52 +0200

    extended import.sh with empty gpkg creation

--- xtf_io/gpkg/import.sh
+++ xtf_io/gpkg/import.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 echo "======================================================="
-echo Imports from import.xtf into data.gpkg
+echo Imports from import.xtf into data.gpkg.
+echo Assumes the geojson is located in ~/code/agi/xtf_io/gpkg.
 echo "Folder with data: $1"
 echo "======================================================="
+
+ogr2ogr -f "GPKG" $1/data.gpkg ~/code/agi/xtf_io/gpkg/gpkg_create_dummy.geojson
 
 java -jar ~/tools/ili2gpkg-5.1.0/ili2gpkg-5.1.0.jar \
 --import --doSchemaImport \
@@ -11,6 +14,7 @@ java -jar ~/tools/ili2gpkg-5.1.0/ili2gpkg-5.1.0.jar \
 $1/import.xtf
 
 echo "======================================================="
-echo Imports from data.xtf into data.gpkg
+echo Imports from import.xtf into data.gpkg.
+echo Assumes the geojson is located in ~/code/agi/xtf_io/gpkg.
 echo "Folder with data: $1"
 echo "======================================================="
